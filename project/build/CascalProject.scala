fca5186ca1feeb90712b1f3ccdf3174a47293594
Shimi Kiviti <shimi.k@gmail.com>
2011-06-04 16:54:45 +0300

    Added sbt eclipsify

--- project/build/CascalProject.scala
+++ project/build/CascalProject.scala
@@ -1,5 +1,6 @@
 import sbt._
+import de.element34.sbteclipsify._
 
-class CascalProject(info:ProjectInfo) extends DefaultProject(info) {
+class CascalProject(info:ProjectInfo) extends DefaultProject(info) with Eclipsify {
   val shorrockin = "Shorrockin Repository" at "http://maven.shorrockin.com"
 }
